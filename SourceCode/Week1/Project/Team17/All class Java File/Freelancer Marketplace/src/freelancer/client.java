f144618b59be7db07890a8268031f67010770b0e
vamshi1421 <167505499+vamshi1421@users.noreply.github.com>
2024-05-24 22:39:14 +0530

    Team 17

--- SourceCode/Week1/Project/Team17/All class Java File/Freelancer Marketplace/src/freelancer/client.java
+++
@@ -1,9 +0,0 @@
-package freelancer;
-
-public class Client {
-    private int clientId;
-    private String clientUsername;
-    private String clientPassword;
-    private String clientEmail;
-    
-}
